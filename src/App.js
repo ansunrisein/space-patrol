7d228782dc2c5f3bfa038c8d532260200fd26ccb
heiNina <nina.shabrova2001@gmail.com>
2020-04-04 23:17:03 +0300

    Main component created

--- src/App.js
+++ src/App.js
@@ -1,26 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "normalize.css";
+import "./index.css";
+import styles from "./App.module.scss";
+import {Header} from "./components/Header";
+import {ArticleCard} from "./components/ArticleCard";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className={styles.container}>
+            <Header/>
+            <main className={styles.articles}>
+                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
+                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
+            </main>
+        </div>
+    );
 }
 
 export default App;
