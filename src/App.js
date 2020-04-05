051aef0622597a6a96315ffbaba65d45c943c0be
heiNina <nina.shabrova2001@gmail.com>
2020-04-06 01:31:02 +0300

    Routes added

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
+import {BrowserRouter, Route, Switch} from "react-router-dom";
 import "normalize.css";
 import "./index.css";
-import styles from "./App.module.scss";
-import {Header} from "./components/Header";
-import {ArticleCard} from "./components/ArticleCard";
+import {HomePage} from "./pages/HomePage";
+import {ArticlePage} from "./pages/ArticlePage"
+
 
 function App() {
     return (
-        <div className={styles.container}>
-            <Header/>
-            <main className={styles.articles}>
-                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun9-44.userapi.com/c543100/v543100492/498d9/MwdWr_kObQQ.jpg"} children={"Космическое пространство"}/>
-                <ArticleCard image={"https://sun1-86.userapi.com/4zf5lpY3mR-xkxnNQoR65-DxJOo6t4-Jz-hcVQ/fWQWTOeOWy8.jpg"} children={"Космическое пространство"}/>
-            </main>
-        </div>
+        <BrowserRouter>
+            <Switch>
+                <Route path="/article/:id" component={ArticlePage}/>
+                <Route exact path="/" component={HomePage}/>
+            </Switch>
+        </BrowserRouter>
     );
 }
 
