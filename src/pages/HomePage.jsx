6d7d0e414b40041ef4eb3fc3af5347add67305b9
heiNina <nina.shabrova2001@gmail.com>
2020-04-08 20:26:54 +0300

    Data fetching to HomePage added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import {useAllArticles} from "../services/ArticleService";
 import styles from "./HomePage.module.scss";
+import {Error} from "./Error";
 import {HomeHeader} from "../components/HomeHeader";
 import {ArticleCard} from "../components/ArticleCard";
-import articles from "../resources/articles";
+import {Loader} from "../components/Loader";
 
-const HomePage = () => (
-    <div className={styles.container}>
-        <HomeHeader/>
-        <main className={styles.articles}> {
-            articles.map(({id, ...props}) => <ArticleCard key={id} id={id} {...props}/>)
-        }</main>
-    </div>
-);
+const HomePage = () => {
+    const {value, pending, error} = useAllArticles();
+
+    if ((!value || error) && !pending)
+        return <Error/>;
+
+    return (
+        <div className={styles.container}>
+            <HomeHeader/>{
+            pending
+                ? <Loader/>
+                : (
+                    <main className={styles.articles}> {
+                        value.map(({_id, ...props}) => <ArticleCard key={_id} id={_id} {...props}/>)
+                    }</main>
+                )
+            }
+        </div>
+    )
+};
 
 export {HomePage};
