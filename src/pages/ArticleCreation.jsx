f58ccde7929d041b4a63d1fc22542320b0d7691d
heiNina <nina.shabrova2001@gmail.com>
2020-04-10 23:07:49 +0300

    Server connection particularly implemented

--- src/pages/ArticleCreation.jsx
+++ src/pages/ArticleCreation.jsx
@@ -1,18 +1,37 @@
 import React from "react";
+import {Field, Form} from "react-final-form";
+import {usePostArticle} from "../services/ArticleService";
 import styles from "./ArticleCreation.module.scss"
 import {content, placeholder} from "../resources/ru";
 import {Button} from "../components/Button";
 import {ImageUpload} from "../components/ImageUpload";
+import {Redirect} from "react-router-dom";
 
-const ArticleCreation = () => (
-    <form>
-        <header className={styles.header}>
-            <ImageUpload/>
-            <input name="name" placeholder={placeholder.title} className={styles.text}/>
-            <Button type="submit" className={styles.button}>{content.save}</Button>
-        </header>
-        <textarea placeholder={placeholder.text} className={styles.article_input}/>
-    </form>
-);
+
+const ArticleCreation = () => {
+    const {value, pending, execute} = usePostArticle();
+
+    if(value)
+        return <Redirect to={`/article/${value._id}`}/>;
+
+    const f = arg => execute({...arg, image: "dffdfdf"});
+
+    return <Form onSubmit={f}>{({handleSubmit}) => <>
+        <form onSubmit={handleSubmit}>
+            <header className={styles.header}>
+                <ImageUpload/>
+                <Field name="title">{({input}) => (
+                    <input placeholder={placeholder.title} className={styles.text} {...input}/>
+                )}</Field>
+                <Button type="submit" className={styles.button} disabled={pending}>{content.save}</Button>
+            </header>
+            <Field name="text">{({input}) => (
+                <textarea placeholder={placeholder.text} className={styles.article_input} {...input}/>
+            )}</Field>
+
+        </form>
+    </>
+    }</Form>
+};
 
 export {ArticleCreation};
